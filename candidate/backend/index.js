479da190ea4974ac07570902933c494e40cbe7a1
Samkit Samsukha <samkitsamsukha@gmail.com>
2025-04-05 10:38:28 +0530

    fix routes and env vars

--- candidate/backend/index.js
+++ candidate/backend/index.js
@@ -1,19 +1,22 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
+import express, { json } from "express";
+import cors from "cors";
+import { config } from "dotenv";
+import { default as connectDB } from "./utils/db.js";
+import candidateRoutes from "./routes/candidateRoutes.js";
 
-dotenv.config();
+config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
+app.use(json());
+
+connectDB();
 
 app.get("/", (req, res) => {
 	res.send("Smart Match API is running...");
 });
 
-const userRoutes = require("./routes/candidateRoutes");
 app.use("/api/candidates", candidateRoutes);
 
 app.listen(PORT, () => {
